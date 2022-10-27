21cbbd57053beb4c08ab82b6cc76c3dd69fa81b7
Epsilon-Aravind <91867114+Epsilon-Aravind@users.noreply.github.com>
2022-10-28 00:37:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+function App() {
+  return (
+    <>
+      <Navbar />
+      <p className='footer'>Aravind G - Copyright &copy; 2022</p>
+    </>
+  )
+}
+
+export default App
